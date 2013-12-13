7de6abec18cd6aefeb801cba0161ea13c16adfd4
Tom Brennan <tommybrennan@gmail.com.com>
2013-12-13 15:33:30 -0500

    Uploaded photos using amazon s3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lesson.destroy_all
+
+Lesson.create(level: "Beginner", type: "Instruction", location: "Aisling Center", datetime: Date.now )
+Lesson.create(level: "Adv Bed & Novice", type: "Instruction", location: "Aisling Center", datetime: Date.now )
+Lesson.create(level: "All Levels", type: "Traditional Set/Hard Shoe", location: "Nepperhan Studio", datetime: Date.now )
