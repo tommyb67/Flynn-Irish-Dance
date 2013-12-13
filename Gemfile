0a3dfadbe0c6bfddedfc2bfa6529cafc872ff7c9
Tom Brennan <tommybrennan@gmail.com.com>
2013-12-13 01:41:59 -0500

    created the lessons table and the welcome controller and view

--- Gemfile
+++ Gemfile
@@ -27,15 +27,18 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-
 gem 'foundation-rails'
 
-group :test, :development, :test do
+gem 'devise'
+
+group :test, :development do
   gem 'rspec-rails'
   gem 'pry-rails'
   gem 'factory_girl_rails'
   gem 'ffaker'
 end
+
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 FlynnIrishDance::Application.routes.draw do
+
+  root 'welcome#index'
+
+  resources :lessons do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +44,7 @@ FlynnIrishDance::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
